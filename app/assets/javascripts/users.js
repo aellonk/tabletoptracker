6f0a026427037b263564009827a4191f6e60f5bd
Aellon Krider <aellonkrider@gmail.com>
2019-02-15 13:41:09 -0500

    Update Users.js with a name for the first function

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,15 +1,25 @@
-$( document ).on('turbolinks:load', function () {
+$(function () {
+  getMatches();
+})
+
+
+
+
+
+
+function getMatches() {
   $("a.userName").on('click', function (e) {
     e.preventDefault();
     let userId = parseInt($(".userName").attr("data-id"));
    	let userURL = ("/users/" + userId + ".json")
 	$.get(userURL, function(data) {
 		let $ul = $("#user-games");
-		let games = data.games;
-      	for (var i = 0; i < games.length; i++) {
-        var game = games[i];
-        $ul.append('<li class="game">'+ game.name +'</li>');
+		let matches = data.matches;
+      	for (var i = 0; i < matches.length; i++) {
+        var match = matches[i];
+        $ul.append('<li class="match">'+ match.game_id +'</li>' +
+                  '<li class="match">'+ match.win +'</li>');
     };
     });
   });
-});
+}
