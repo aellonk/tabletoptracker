92f1a23299e2e3f923dcc469f015e303e8ab507c
Aellon Krider <aellonkrider@gmail.com>
2019-02-28 14:40:04 -0500

    Update games.js and games show page to apend game info and include a link to the next game

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -23,18 +23,23 @@ function getGameInfo() {
 	    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
 	    $.get("/games/" + nextId + ".json", function(data) {
 	    	
-	    	$(".gameName").append('<p class="game">'+data.name+'</p>');
+	    	$(".gameName").append('<h3 class="game">'+data.name+'</h3>');
 	      	
 
 	      // for (var i = 0; i < data.matches.length; i++) {
        //  	var match = match[i];
        //  	$(".matchDate").append('<p class="match">'+match.match_date+'</p>');
     	  // };
-
+ 
 
 	data.matches.map(match => {
-      $(".gameName").append('<li>' + match.match_date + '</li><li>' + match.user_id + '</li>');
+		let matchUser = data.users.map(user => {
+     		$(".gameName").append('<p>User: ' + user.name + '</p><p>Match Date: '+ match.match_date + '</p><p>Won? ' + match.win + '</p><br>');
+    	})
+      
     })
+
+  
 	     
 	      // re-set the id to current on the link
 	      $(".js-next").attr("data-id", data["id"]);
