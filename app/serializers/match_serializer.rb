1d7c56b58d797254fa5debc7bc63bdb350900ff5
Aellon Krider <aellonkrider@gmail.com>
2019-02-28 21:33:02 -0500

    add scythe to match serializer

--- app/serializers/match_serializer.rb
+++ app/serializers/match_serializer.rb
@@ -2,4 +2,5 @@ class MatchSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :game_id, :match_date, :win
   belongs_to :game
   belongs_to :user
+  has_one :scythe, inverse_of: :match, :dependent => :destroy
 end
