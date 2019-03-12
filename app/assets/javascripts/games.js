aafde995cc3e6fdb63b6ebb748c25a6445a55caa
Aellon Krider <aellonkrider@gmail.com>
2019-03-12 12:42:21 -0400

    Fix games js to show correctly on the games show page

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -12,14 +12,35 @@ function getNextGameInfo() {
 	    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
 	    $.get("/games/" + nextId + ".json", function(data) {
 	    	
-	    	$(".gameName").append(`<section class="jumbotron text-center"><div class="container"><h1 class="jumbotron-heading">${data.name}</h1></div></section>`);
+	    $(".gameName").append(`
+	    	<section class="jumbotron text-center">
+    		<div class="container">
+    		<h1 class="jumbotron-heading">${data.name}</h1>
+			</div>
+			</section>`);
 
-	data.matches.map(match => {
-		let matchUser = data.users.map(user => {
-     		$(".gameName").append(`<div class="album py-5 bg-light"><div class="container"><div class="row"><div class="col-md-4"><div class="card mb-4 shadow-sm"><div class="card-body"><p class="card-text">Player: ${user.name} </p><p class="card-text">Match Date: ${ match.match_date}</p><p class="card-text">Win? ${match.win}</p></div></div></div></div></div>`);
-    	})
-      
-    })
+		data.matches.map(match => {
+			let matchUser = data.users.map(user => {
+	     		$(".gameName").append(`
+	     			<div class="album py-5 bg-light">
+					<div class="container">
+					<div class="row">
+	     			<div class="col-md-12">
+	     			<div class="card mb-4 shadow-sm">
+	     			<div class="card-body">
+	     			<p class="card-text">Player: ${user.name} </p>
+	     			<p class="card-text">Match Date: ${ match.match_date}</p>
+	     			<p class="card-text">Win? ${match.win}</p>
+	     			</div>
+	     			</div>
+	     			</div>
+	     			</div>
+	     			</div>
+	     			</div>
+	     		`);
+	    	})
+	      
+	    })
 	     
 	      // re-set the id to current on the link
 	      $(".js-next").attr("data-id", data["id"]);
--- app/assets/javascripts/matches.js
+++ app/assets/javascripts/matches.js
@@ -1,20 +1,19 @@
 $(function () {
-	getNewMatch()
+	submitNewMatch()
  })
 
 
-function getNewMatch() {
+function submitNewMatch() {
   $("form").submit(function (e) {
      e.preventDefault()
 
      let gameId = parseInt($(".gameName").attr("data-id"));
      
-	var values = $(this).serialize();
-	var posting = $.post('/games/' + gameId + '/matches', values);
-    posting.done(function(data) {
-    let $div = $("#new-match-response");
-	$div.append(data)    
-
+	   var values = $(this).serialize();
+	   var posting = $.post('/games/' + gameId + '/matches', values);
+     posting.done(function(data) {
+     let $div = $("#new-match-response");
+	   $div.append(data)    
 	})
   })
 }
